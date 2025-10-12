d5312e4f0aa76e47619ece32bede80957ec28662
MeghlaB <meghlab964@gmail.com>
2025-10-13 02:59:40 +0600

    controllers with user controllers

---
+++ src/services/user.service.ts
@@ -0,0 +1,25 @@
+import { IUser } from "../interfaces/IUser";
+import { User } from "../models/User.model";
+
+
+
+//  all User get
+export const getAllUsers = async (): Promise<IUser[]> => {
+    try {
+        const users = await User.find()
+        return users
+    } catch (error) {
+        throw new Error('Falied to detch users')
+    }
+}
+
+// Create By new User
+export const createUser = async (userData: Partial<IUser>): Promise<IUser> => {
+  try {
+    const newUser = new User(userData);
+    await newUser.save();
+    return newUser;
+  } catch (error) {
+    throw new Error("Failed to create user");
+  }
+};
